ac21b50a13a2cfb0efee1a452c9b1cca729796eb
msulthon <59869908+msulthon@users.noreply.github.com>
2020-01-23 18:22:54 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 
 /** WRITE DOWN YOUR INFORMATION HERE */
-string name = ""; // put your name here
-string ID = ""; // put your student id here
-int group_id = 0; // your Group Number here (1-8)
+string name = "Muhammad Sulthon Asramanggala"; // put your name here
+string ID = "1301194008"; // put your student id here
+int group_id = 2; // your Group Number here (1-8)
 
 
 /** FUNCTIONS LIST, DO NOT MODIFY THESE */
@@ -150,7 +150,7 @@ string count_and_sum(int arr[], int n) {
 
     // YOUR CODES HERE
     //-----------------------
-
+        
 
     //-----------------------
     return "";
